4f6a74e5b8586e9039bcfa7154d2eed4cb054dc0
hibohiboo <hibohiboo66+github@gmail.com>
2024-07-05 00:27:45 +0900

    add 再起関数

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,20 @@ fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64,f64)
     context.stroke();
 }
 
+fn sierpinsk(context: &web_sys::CanvasRenderingContext2d, points: [(f64,f64);3], depth: u8) {
+  if depth == 0 {
+    draw_triangle(context, points);
+  } else {
+    let [top, left, right] = points;
+    let left_mid = ((top.0 + left.0) / 2.0, (top.1 + left.1) / 2.0);
+    let right_mid = ((top.0 + right.0) / 2.0, (top.1 + right.1) / 2.0);
+    let bottom_mid = ((left.0 + right.0) / 2.0, (left.1 + right.1) / 2.0);
+
+    sierpinsk(context, [top, left_mid, right_mid], depth - 1);
+    sierpinsk(context, [left_mid, left, bottom_mid], depth - 1);
+    sierpinsk(context, [right_mid, bottom_mid, right], depth - 1);
+  }
+}
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
@@ -22,10 +36,8 @@ pub fn main_js() -> Result<(), JsValue> {
     let canvas = document.get_element_by_id("canvas").unwrap().dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
     let context = canvas.get_context("2d").unwrap().unwrap().dyn_into::<web_sys::CanvasRenderingContext2d>().unwrap();
     
-    draw_triangle(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)]);
-    draw_triangle(&context, [(300.0, 0.0), (150.0, 300.0), (450.0, 300.0)]);
-    draw_triangle(&context, [(150.0, 300.0), (0.0, 600.0), (300.0, 600.0)]);
-    draw_triangle(&context, [(450.0, 300.0), (300.0, 600.0), (600.0, 600.0)]);
+    sierpinsk(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],5);
+
 
     
     Ok(())
