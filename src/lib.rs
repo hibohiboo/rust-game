059df1585778ab686ba412fbc2e1208bb2db2810
hibohiboo <hibohiboo66+github@gmail.com>
2024-07-05 00:09:35 +0900

    add draw triangle AB#481

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,24 @@
+use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
-
-// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
-// allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-
-// This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap().dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
+    let context = canvas.get_context("2d")?.unwrap().dyn_into::<web_sys::CanvasRenderingContext2d>().unwrap();
 
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
-
+    context.move_to(300.0, 0.0); // 上
+    context.begin_path();
+    context.line_to(0.0, 600.0); // 左下
+    context.line_to(600.0, 600.0); // 右下
+    context.line_to(300.0, 0.0); // 上に戻る
+    context.close_path();
+    context.stroke();
+    context.fill();
+    
     Ok(())
 }
