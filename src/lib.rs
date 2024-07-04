13bc5d0af3ad36ec98a8c1cd45fa9813c15e810c
hibohiboo <hibohiboo66+github@gmail.com>
2024-07-05 00:21:52 +0900

    fix 関数化

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
 use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::*;
-use web_sys::console;
+
+fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64,f64);3]) {
+  let [top, left, right] = points;
+
+    context.move_to(top.0, top.1);
+    context.begin_path();
+    context.line_to(left.0, left.1);
+    context.line_to(right.0, right.1);
+    context.line_to(top.0, top.1);
+    context.close_path();
+    context.stroke();
+}
 
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
@@ -10,15 +21,8 @@ pub fn main_js() -> Result<(), JsValue> {
     let document = window.document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap().dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
     let context = canvas.get_context("2d").unwrap().unwrap().dyn_into::<web_sys::CanvasRenderingContext2d>().unwrap();
-
-    context.move_to(300.0, 0.0); // 上
-    context.begin_path();
-    context.line_to(0.0, 600.0); // 左下
-    context.line_to(600.0, 600.0); // 右下
-    context.line_to(300.0, 0.0); // 上に戻る
-    context.close_path();
-    context.stroke();
-    context.fill();
+    
+    draw_triangle(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)]);
     
     Ok(())
 }
