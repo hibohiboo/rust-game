7fe67af9a733d983d890565c57f033ecefbf2a88
hibohiboo <hibohiboo66+github@gmail.com>
2024-07-06 16:24:06 +0900

    delete 不要になるコード AB#485

--- src/lib.rs
+++ src/lib.rs
@@ -28,66 +28,10 @@ struct Sheet {
 }
 
 
-fn draw_triangle(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-) {
-    let [top, left, right] = points;
 
-    let color_str = format!("rgb({},{},{})", color.0, color.1, color.2);
-    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
-
-    context.move_to(top.0, top.1);
-    context.begin_path();
-    context.line_to(left.0, left.1);
-    context.line_to(right.0, right.1);
-    context.line_to(top.0, top.1);
-    context.close_path();
-    context.stroke();
-    context.fill();
-}
-
-fn midpoint(p1: (f64, f64), p2: (f64, f64)) -> (f64, f64) {
-    ((p1.0 + p2.0) / 2.0, (p1.1 + p2.1) / 2.0)
-}
-
-fn sierpinsk(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-    depth: u8,
-) {
-    draw_triangle(context, points, color);
-    let depth = depth -1;
-    let [top, left, right] = points;
-    if depth > 0 {
-        let mut rng = rand::thread_rng();
-        let next_color = (
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-        );
-
-       
-        let left_mid = midpoint(top, left);
-        let right_mid = midpoint(top, right);
-        let bottom_mid = midpoint(left, right);
-
-        sierpinsk(context, [top, left_mid, right_mid], next_color, depth);
-        sierpinsk(context, [left_mid, left, bottom_mid], next_color, depth);
-        sierpinsk(
-            context,
-            [right_mid, bottom_mid, right],
-            next_color,
-            depth,
-        );
-    }
-}
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
-
 
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
@@ -103,32 +47,7 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
     wasm_bindgen_futures::spawn_local(async move {
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
-        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
-        let error_tx = Rc::clone(&success_tx);
 
-        let image = web_sys::HtmlImageElement::new().unwrap();
-        let callback = Closure::once(move||{
-            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                success_tx.send(Ok(()));
-            }
-        });
-        let error_callback = Closure::once(move|err|{
-            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                error_tx.send(Err(err));
-            }
-        });
-        image.set_onload(Some(callback.as_ref().unchecked_ref()));
-        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-        image.set_src("Idle (1).png");
-        success_rx.await;
-        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
-        sierpinsk(
-            &context,
-            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-            (0, 255, 0),
-            5,
-        );
         // JSONファイルをロードする
         let json = fetch_json("rhb.json").await.expect("Failed to fetch JSON");
         // JSONファイルをパースしてRustの構造体にする
