60093146beddfd4df3372a228e2b246c3b40e997
hibohiboo <hibohiboo66+github@gmail.com>
2024-07-07 23:14:18 +0900

    add fetch json AB#489

--- src/browser.rs
+++ src/browser.rs
@@ -1,6 +1,7 @@
 use anyhow::{anyhow, Result};
-use web_sys::{Window, Document, HtmlCanvasElement, CanvasRenderingContext2d};
-use wasm_bindgen::JsCast;
+use wasm_bindgen::{JsCast, JsValue};
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{CanvasRenderingContext2d, Document, HtmlCanvasElement, Response, Window};
 
 macro_rules! log {
     ($($t:tt)*) => {
@@ -8,26 +9,54 @@ macro_rules! log {
     };
 }
 
-pub fn window() -> Result<Window>{
+pub fn window() -> Result<Window> {
     web_sys::window().ok_or_else(|| anyhow!("No Window Founc"))
 }
 
-pub fn document() -> Result<Document>{
-    window()?.document().ok_or_else(|| anyhow!("No Document Founc"))
+pub fn document() -> Result<Document> {
+    window()?
+        .document()
+        .ok_or_else(|| anyhow!("No Document Founc"))
 }
 
-pub fn canvas() -> Result<HtmlCanvasElement>{
+pub fn canvas() -> Result<HtmlCanvasElement> {
     document()?
         .get_element_by_id("canvas") // "canvas"がハードコーディングされているが必要になるまで再設定可能にするのはやめる
         .ok_or_else(|| anyhow!("No Canvas Element Found with ID 'canvas'"))?
         .dyn_into::<web_sys::HtmlCanvasElement>()
         .map_err(|element| anyhow!("Error convrting {:#?} to HtmlCanvasElement", element))
 }
-pub fn context() -> Result<CanvasRenderingContext2d>{
+pub fn context() -> Result<CanvasRenderingContext2d> {
     canvas()?
         .get_context("2d")
-        .map_err(|js_value| anyhow!("Error getting 2d context {:#?}",js_value))?
+        .map_err(|js_value| anyhow!("Error getting 2d context {:#?}", js_value))?
         .ok_or_else(|| anyhow!("Failed to get 2d context"))?
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .map_err(|context| anyhow!("Error converting {:#?} to CanvasRenderingContext2d", context))
+        .map_err(|context| {
+            anyhow!(
+                "Error converting {:#?} to CanvasRenderingContext2d",
+                context
+            )
+        })
+}
+
+pub fn spawn_local<F>(future: F)
+where
+    F: std::future::Future<Output = ()> + 'static,
+{
+    wasm_bindgen_futures::spawn_local(future);
+}
+
+pub async fn fetch_with_str(resource: &str) -> Result<JsValue> {
+    JsFuture::from(window()?.fetch_with_str(resource))
+        .await
+        .map_err(|err| anyhow!("Failed to fetch resource: {:#?}", err))
+}
+
+pub async fn fetch_json(json_path: &str) -> Result<JsValue> {
+    let response = fetch_with_str(json_path).await?;
+    let res : Response = response.dyn_into().map_err(|element| anyhow!("Error converting {:#?} to Response", element))?;
+    JsFuture::from(res.json().map_err(|err| anyhow!("Failed to get JSON: {:#?}", err))?,)   
+        .await
+        .map_err(|err| anyhow!("Failed to parse JSON: {:#?}", err))
 }
