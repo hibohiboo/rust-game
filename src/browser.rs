498ca8006b206b408cc9b6a0b2b9dd757b18ff5e
hibohiboo <hibohiboo66+github@gmail.com>
2024-07-07 22:51:40 +0900

    add canvas macro AB#489

--- src/browser.rs
+++ src/browser.rs
@@ -1,5 +1,6 @@
 use anyhow::{anyhow, Result};
 use web_sys::{Window, Document, HtmlCanvasElement, CanvasRenderingContext2d};
+use wasm_bindgen::JsCast;
 
 macro_rules! log {
     ($($t:tt)*) => {
@@ -13,4 +14,12 @@ pub fn window() -> Result<Window>{
 
 pub fn document() -> Result<Document>{
     window()?.document().ok_or_else(|| anyhow!("No Document Founc"))
+}
+
+pub fn canvas() -> Result<HtmlCanvasElement>{
+    document()?
+        .get_element_by_id("canvas") // "canvas"がハードコーディングされているが必要になるまで再設定可能にするのはやめる
+        .ok_or_else(|| anyhow!("No Canvas Element Found with ID 'canvas'"))?
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|element| anyhow!("Error convrting {:#?} to HtmlCanvasElement", element))
 }
