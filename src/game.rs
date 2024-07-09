cfd61a5ce62f6e403f9a24e329cca7c83b48015d
hibohiboo <hibohiboo66+github@gmail.com>
2024-07-10 00:48:43 +0900

    fix frame AB#496

--- src/game.rs
+++ src/game.rs
@@ -117,6 +117,31 @@ impl RedHatBody {
             image,
         }
     }
+
+    fn draw(&self, renderer: Renderer) {
+        let current_sprite = (self.state_machine.context().frame / 3) + 1;
+        let frame_name = format!("{} ({}).png", self.state_machine.frame_name(), current_sprite);
+        let sprite = self
+            .sprite_sheet
+            .frames.get(&frame_name)
+            .expect("Cell not found in sheet");
+        renderer.draw_image(
+            &self.image,
+            &Rect {
+                x: sprite.frame.x.into(),
+                y: sprite.frame.y.into(),
+                width: sprite.frame.w.into(),
+                height: sprite.frame.h.into(),
+            },
+            &Rect {
+                x: self.state_machine.context().position.x.into(),
+                y: self.state_machine.context().position.y.into(),
+                width: sprite.frame.w.into(),
+                height: sprite.frame.h.into(),
+            },
+        );
+
+    }
 }
 #[derive(Copy, Clone)]
 enum RedHatBoyStateMachine {
@@ -127,6 +152,8 @@ enum RedHatBoyStateMachine {
 
 mod red_hat_boy_states {
     use crate::engine::Point;
+    const IDLE_FRAME_NAME: &str = "Idle";
+    const RUN_FRAME_NAME: &str = "Run";
 
     #[derive(Copy, Clone)]
     pub struct RedHatBoyState<S> {
@@ -134,17 +161,24 @@ mod red_hat_boy_states {
         _state: S,
     }
 
+    impl<S> RedHatBoyState<S> {
+        pub fn context(&self) -> &RedHatBoyContext {
+            &self.context
+        }
+    }
+
     #[derive(Copy, Clone)]
     pub struct RedHatBoyContext {
-        frame: u8,
-        position: Point,
-        velocity: Point,
+        pub frame: u8,
+        pub position: Point,
+        pub velocity: Point,
     }
 
     #[derive(Copy, Clone)]
     pub struct Idle;
     #[derive(Copy, Clone)]
     pub struct Running;
+
     const FLOOR: i16 = 479;
 
     impl RedHatBoyState<Idle> {
@@ -164,8 +198,16 @@ mod red_hat_boy_states {
                 _state: Running {},
             }
         }
+        pub fn frame_name(&self) -> &str {
+            IDLE_FRAME_NAME
+        }
     }
 
+    impl RedHatBoyState<Running> {
+        pub fn frame_name(&self) -> &str {
+            RUN_FRAME_NAME
+        }
+    }
     #[derive(Copy, Clone)]
     enum RedHatBoyStateMachine {
         Idle(RedHatBoyState<Idle>),
@@ -184,6 +226,18 @@ impl RedHatBoyStateMachine {
         match (self, event) {
             (RedHatBoyStateMachine::Idle(state), Event::Run) => state.run().into(),
             _ => self,
+        }
+    }
+    fn frame_name(&self) -> &str {
+        match self {
+            RedHatBoyStateMachine::Idle(state) => state.frame_name(),
+            RedHatBoyStateMachine::Running(state) => state.frame_name(),
+        }
+    }
+    fn context(&self) -> &RedHatBoyContext {
+        match self {
+            RedHatBoyStateMachine::Idle(state) => &state.context(),
+            RedHatBoyStateMachine::Running(state) => &state.context(),
         }
     }
 }
