f0731941aaf69719066ca23383822ae35b3e4775
hibohiboo <hibohiboo66+github@gmail.com>
2024-07-09 19:56:29 +0900

    add RHBが十字キーで動くようになった AB#492

--- src/game.rs
+++ src/game.rs
@@ -1,15 +1,17 @@
 use std::collections::HashMap;
 
-use crate::{browser, engine::{self, Game, KeyState, Point, Rect, Renderer}};
+use crate::{
+    browser,
+    engine::{self, Game, KeyState, Point, Rect, Renderer},
+};
 use anyhow::Result;
 use async_trait::async_trait;
 use gloo_utils::format::JsValueSerdeExt;
 use serde::Deserialize;
 use web_sys::HtmlImageElement;
 
-
 #[derive(Deserialize)]
-struct SheetRect{
+struct SheetRect {
     x: i16,
     y: i16,
     w: i16,
@@ -26,7 +28,7 @@ struct Sheet {
     frames: HashMap<String, Cell>,
 }
 
-pub struct WalkTheDog{
+pub struct WalkTheDog {
     image: Option<HtmlImageElement>,
     sheet: Option<Sheet>,
     frame: u8,
@@ -48,7 +50,7 @@ impl Game for WalkTheDog {
     async fn initialize(&self) -> Result<Box<dyn Game>> {
         let sheet = browser::fetch_json("rhb.json").await?.into_serde()?;
         let image = Some(engine::load_image("rhb.png").await?);
-        Ok(Box::new(WalkTheDog{
+        Ok(Box::new(WalkTheDog {
             image: image,
             sheet: sheet,
             position: self.position,
@@ -56,16 +58,38 @@ impl Game for WalkTheDog {
         }))
     }
     fn update(&mut self, keystate: &KeyState) {
-       if self.frame < 23 {
-           self.frame += 1;
-       } else {
-           self.frame = 0;
-       }
+        let mut velocity = Point { x: 0, y: 0 };
+        // 同時押しに対応するため、elseを使わずifで分岐している
+        if (keystate.is_pressed("ArrowDown")) {
+            velocity.y += 3;
+        }
+        if (keystate.is_pressed("ArrowUp")) {
+            velocity.y -= 3;
+        }
+        if (keystate.is_pressed("ArrowLeft")) {
+            velocity.x -= 3;
+        }
+        if (keystate.is_pressed("ArrowRight")) {
+            velocity.x += 3;
+        }
+
+        self.position.x += velocity.x;
+        self.position.y += velocity.y;
+
+        if self.frame < 23 {
+            self.frame += 1;
+        } else {
+            self.frame = 0;
+        }
     }
     fn draw(&self, renderer: &Renderer) {
-        let current_sprite = (self.frame/3)+1;
+        let current_sprite = (self.frame / 3) + 1;
         let frame_name = format!("Run ({}).png", current_sprite);
-        let sprite = self.sheet.as_ref().and_then(|sheet| sheet.frames.get(&frame_name)).expect("Cell not found in sheet");
+        let sprite = self
+            .sheet
+            .as_ref()
+            .and_then(|sheet| sheet.frames.get(&frame_name))
+            .expect("Cell not found in sheet");
         renderer.clear(&Rect {
             x: 0.0,
             y: 0.0,
@@ -73,18 +97,21 @@ impl Game for WalkTheDog {
             height: 600.0,
         });
         self.image.as_ref().map(|image| {
-            renderer.draw_image(&image, &Rect {
-                x: sprite.frame.x.into(),
-                y: sprite.frame.y.into(),
-                width: sprite.frame.w.into(),
-                height: sprite.frame.h.into(),
-            }, &Rect {
-                x: self.position.x.into(),
-                y: self.position.y.into(),
-                width: sprite.frame.w.into(),
-                height: sprite.frame.h.into(),
-            });
+            renderer.draw_image(
+                &image,
+                &Rect {
+                    x: sprite.frame.x.into(),
+                    y: sprite.frame.y.into(),
+                    width: sprite.frame.w.into(),
+                    height: sprite.frame.h.into(),
+                },
+                &Rect {
+                    x: self.position.x.into(),
+                    y: self.position.y.into(),
+                    width: sprite.frame.w.into(),
+                    height: sprite.frame.h.into(),
+                },
+            );
         });
-        
     }
 }
