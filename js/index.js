f4828220a24131c6c720761de4455010ed118aec
hibohiboo <hibohiboo66+github@gmail.com>
2024-07-14 01:52:12 +0900

    fix jump button AB#518

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
 import("../pkg/index.js").catch(console.error);
 const button = document.querySelector('#ui button');
-button.addEventListener('click', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    const canvas = document.querySelector('#canvas');
+const canvas = document.querySelector('#canvas');
+button.addEventListener('click', () => {
     canvas.focus();
     canvas.dispatchEvent(new KeyboardEvent('keydown', {code: 'ArrowRight'}));
     button.remove();
+});
+document.querySelector('#jump').addEventListener('pointerdown', () => {
+    canvas.focus();
+    canvas.dispatchEvent(new KeyboardEvent('keydown', {code: 'Space'}));
+
+});
+document.querySelector('#jump').addEventListener('pointerup', () => {
+    canvas.focus();
+    canvas.dispatchEvent(new KeyboardEvent('keyup', {code: 'Space'}));
+});
+document.querySelector('#sliding').addEventListener('pointerdown', () => {
+    canvas.focus();
+    canvas.dispatchEvent(new KeyboardEvent('keydown', {code: 'ArrowDown'}));
+});
+document.querySelector('#sliding').addEventListener('pointerup', () => {
+    canvas.focus();
+    canvas.dispatchEvent(new KeyboardEvent('keyup', {code: 'ArrowDown'}));
 });
